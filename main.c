908a2cb631facac3e38f24e7494a35f3fe530630
Nikita Milenin <rtsocnm@gmail.com>
2023-10-25 15:11:44 +0000

    Added some improvements to code

--- main.c
+++ main.c
@@ -53,6 +53,14 @@
 #include "nrf_delay.h"
 #include "boards.h"
 
+#define SMALL_DELAY 400
+#define BIG_DELAY 1000
+#define VERY_BIG_DELAY 2000
+
+void blink(int);
+void blink_n_times(int, int);
+
+
 /**
  * @brief Function for application main entry.
  */
@@ -66,35 +74,27 @@ int main(void)
     {
         for (int i = 0; i < LEDS_NUMBER; i++)
         {
-            int j;
-            for (j = 0; j < 6; j++) {
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-            }
-            nrf_delay_ms(1000);
-            for (j = 0; j < 5; j++) {
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-            }
-            nrf_delay_ms(1000);
-            for (j = 0; j < 7; j++) {
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-            }
-            nrf_delay_ms(1000);
-            for (j = 0; j < 9; j++) {
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-                bsp_board_led_invert(i);
-                nrf_delay_ms(400);
-            }
-            nrf_delay_ms(2000);
+            blink_n_times(i, 6);
+            blink_n_times(i, 5);
+            blink_n_times(i, 7);
+            blink_n_times(i, 9);
+            nrf_delay_ms(VERY_BIG_DELAY);
         }
     }
+}
+
+void blink(int led_id)
+{
+    bsp_board_led_invert(led_id);
+    nrf_delay_ms(SMALL_DELAY);
+    bsp_board_led_invert(led_id);
+    nrf_delay_ms(SMALL_DELAY);
+}
+
+void blink_n_times(int led_id, int n)
+{
+    for (int j = 0; j < n; j++) {
+        blink(led_id);
+    }
+    nrf_delay_ms(BIG_DELAY);
 }
